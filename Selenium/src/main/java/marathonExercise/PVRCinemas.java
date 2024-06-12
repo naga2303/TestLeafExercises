8048cfca9105ffd81e8ae6a61d895c878553b8ea
nagarajan <nagarajan.r@volantetech.com>
2024-06-12 17:11:39 +0530

    commits

---
+++ Selenium/src/main/java/marathonExercise/PVRCinemas.java
@@ -0,0 +1,12 @@
+package marathonExercise;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class PVRCinemas
+{
+    public static void main(String args[]){
+        ChromeDriver driver = new ChromeDriver();
+        driver.get("https://www.pvrcinemas.com/");
+
+    }
+}
---
+++ Selenium/src/main/java/weeklyTest/JuneExercise/ctest.java
@@ -0,0 +1,7 @@
+package weeklyTest.JuneExercise;
+
+public class ctest {
+        public static void main(String args[]){
+       // System.out.println(args[1]) + args[2] + args[3]);
+        }
+}
