592f705f37a46a905e80b7f483931aebfa3394ae
nagarajan <nagarajan.r@volantetech.com>
2024-06-13 14:22:09 +0530

    last word pgm

---
+++ Selenium/src/main/java/weeklyTest/JuneExercise/LastWord.java
@@ -0,0 +1,23 @@
+package weeklyTest.JuneExercise;
+
+import java.sql.Array;
+
+public class LastWord
+{
+    public static void main(String args[])
+    {
+        LastWord lw = new LastWord();
+        System.out.println(lw.lengthLastWord("Hello World"));
+        System.out.println(lw.lengthLastWord(" fly me to the moon "));
+        System.out.println(lw.lengthLastWord("luffy is still joyboy"));
+    }
+
+    public int lengthLastWord(String str){
+        String text = str.trim();
+        String[] ar = new String[str.length()];
+        ar = text.split(" ");
+        int value;
+        value = ar[ar.length-1].length();
+        return value;
+    }
+}
