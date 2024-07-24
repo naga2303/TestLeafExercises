bb7688305cc4f8f1c94fdd6ec468b68587e79be0
nagarajan <nagarajan.r@volantetech.com>
2024-07-24 13:59:40 +0530

    last word pgm

--- Selenium/src/main/java/javaExercises/Strings/ReverseOnlyEnglish.java
+++ Selenium/src/main/java/javaExercises/Strings/ReverseOnlyEnglish.java
@@ -1,39 +1,39 @@
 package javaExercises.Strings;
 
+import org.checkerframework.checker.units.qual.C;
+
 import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class ReverseOnlyEnglish
-{
-    public static void main(String args[]){
-       System.out.println( ReverseOnlyEnglish.reverseEnglish("ab-cd"));
-        System.out.println(ReverseOnlyEnglish.reverseEnglish("Testleaf-Software=Great-Value"));
+public class ReverseOnlyEnglish {
+    public static void main(String args[]) {
+        System.out.println(ReverseOnlyEnglish.reverseEnglish("ab-cd"));
+        System.out.println(ReverseOnlyEnglish.reverseEnglish("Testleaf-Software=Great-Value!"));
     }
-    public static String reverseEnglish(String chars){
-        String rev="";
-        String match = "";
 
-        String regex = "[a-zA-Z]*";
-        Pattern pattern = Pattern.compile(regex);
-
-        Matcher matcher = pattern.matcher(chars);
-
-        boolean isMatch = matcher.matches();
-        if(isMatch) {
-            while (matcher.find()) {
-                match = matcher.group();
-                System.out.println(match);
-
-            }
-            rev = rev + match;
+    public static String reverseEnglish(String str) {
+        StringBuilder sb = new StringBuilder();
+        char[] ch = str.toCharArray();
+        boolean bool=false;
+        int len = 0;
+        if (!Character.isLetter(str.charAt(str.length()-1))) {
+            len = str.length() - 2;
+            bool=true;
         }
         else
-            {
-                rev = rev+ chars;
-            }
+        {
+            len = str.length()-1;
+        }
+        for (int i = len; i >= 0; i--) {
+            sb = sb.append(str.charAt(i));
+        }
+        if(bool)
+        {
+           sb= sb.append(str.charAt(str.length()-1));
+        }
+        return sb.toString();
 
-
-        return rev;
     }
 }
+
